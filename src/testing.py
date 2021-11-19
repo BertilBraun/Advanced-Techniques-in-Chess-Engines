6043b1494823d17ae7f85a961749cd84ddd2ee62
Bertil <bertil.braun.private@gmail.com>
2021-11-19 18:33:42 +0100

    Only select valid weight files

--- src/testing.py
+++ src/testing.py
@@ -6,7 +6,9 @@ if __name__ == "__main__":
     model = gen_model()
 
     # get the last filename in the sorted directory "../training"
-    last_file = sorted(os.listdir("../training"))[-1]
+    last_file = sorted([
+        f for f in os.listdir("../training") if f.endswith('.h5')
+    ])[-1]
     print("Loading weights from: ../training/" + last_file)
 
     model.load_weights("../training/" + last_file)
