d56a773a19101b78b64b208502918871cbf49a2e
Bertil <bertil.braun.private@gmail.com>
2022-01-27 23:57:18 +0100

    split and cleanup into util file

--- src/nn.py
+++ src/nn.py
@@ -3,136 +3,19 @@ import os
 from bz2 import BZ2File
 from io import StringIO
 from time import time
-from typing import List, TextIO, Tuple
+from typing import TextIO
 
 import chess
 import chess.pgn
-import numpy as np
 import pandas as pd
-from pandas.core.frame import DataFrame
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.layers import (BatchNormalization, Conv2D, Conv3D, Dense,
-                                     Flatten, Rescaling, Reshape)
+                                     Flatten, Reshape)
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 from tensorflow.python.keras.callbacks import Callback, History, TensorBoard
 
-
-def board_to_bitfields(board: chess.Board, turn: chess.Color) -> np.ndarray:
-
-    pieces_array = []
-    colors = [chess.WHITE, chess.BLACK]
-    for c in colors if turn == chess.WHITE else colors[::-1]:
-        for p in (chess.PAWN, chess.KNIGHT, chess.BISHOP, chess.ROOK, chess.QUEEN, chess.KING):
-            pieces_array.append(board.pieces_mask(p, c))
-
-    return np.array(pieces_array).astype(np.int64)
-
-
-def bitfield_to_nums(bitfield: np.int64, white: bool) -> np.ndarray:
-
-    board_array = np.zeros(64).astype(np.float32)
-
-    for i in np.arange(64).astype(np.int64):
-        if bitfield & (1 << i):
-            board_array[i] = 1. if white else -1.
-
-    return board_array
-
-
-def bitfields_to_nums(bitfields: np.ndarray) -> np.ndarray:
-    bitfields = bitfields.astype(np.int64)
-
-    boards = []
-
-    for i, bitfield in enumerate(bitfields):
-        boards.append(bitfield_to_nums(bitfield, i < 6))
-
-    return np.array(boards).astype(np.float32)
-
-
-def board_to_nums(board: chess.Board, turn: chess.Color) -> np.ndarray:
-
-    return bitfields_to_nums(board_to_bitfields(board, turn))
-
-
-def pager(in_file: TextIO, lines_per_page=20):
-    assert lines_per_page > 1 and lines_per_page == int(lines_per_page)
-
-    lin_ctr = 0
-    current = ""
-    for lin in in_file:
-        lin_ctr += 1
-        current += lin.decode("utf-8") + "\n"
-        if lin_ctr % lines_per_page == 0:
-            yield current
-            current = ""
-            if lin_ctr % (lines_per_page * 5000) == 0:
-                print("Next:" + str(lin_ctr // lines_per_page))
-
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-
-def create_training_data(dataset: DataFrame) -> Tuple[np.ndarray, np.ndarray]:
-    def drop(indices, fract):
-        drop_index = np.random.choice(
-            indices,
-            size=int(len(indices) * fract),
-            replace=False)
-        dataset.drop(drop_index, inplace=True)
-
-    drop(dataset[abs(dataset[12] / 10.) > 30].index, fract=0.80)
-    drop(dataset[abs(dataset[12] / 10.) < 0.1].index, fract=0.90)
-    drop(dataset[abs(dataset[12] / 10.) < 0.15].index, fract=0.10)
-
-    y = dataset[12].values
-    X = dataset.drop(12, axis=1)
-
-    def transform(row):
-        return list(np.concatenate(bitfields_to_nums(row)))
-    X = X.apply(transform, axis=1, result_type='expand')
-
-    # move into range of -1 to 1
-    y = y.astype(np.float32)
-    y = np.tanh(y / 10.)
-    # y = sigmoid(y / 10.)
-    print(min(y), max(y))
-    print(X.shape, y.shape)
-
-    return X, y
-
-
-def plot_history(history: History, index):
-    plot(history.history['loss'], history.history['val_loss'], 'loss', index)
-    # plot(history.history['accuracy'], history.history['val_accuracy'], 'accuracy', index)
-
-
-def plot(data: List, val_data: List, type: str, index):
-    import matplotlib.pyplot as plt
-
-    plt.plot(data)
-    plt.plot(val_data)
-    plt.title(f'model {type}')
-    plt.ylabel(type)
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.savefig(f'training/{type}{index}.png')
-
-    plt.clf()
-
-
-def load_last_training_weights_file(model: Sequential) -> None:
-
-    # get the last filename in the sorted directory "training"
-    last_files = sorted([
-        f for f in os.listdir("training") if f.endswith('.h5')
-    ])
-
-    if len(last_files) > 0:
-        model.load_weights(f"training/{last_files[-1]}")
-
+from util import *
 
 out_files = {}
 
@@ -170,10 +53,16 @@ def process_game(lines: str, out_path: str) -> None:
 
 def preprocess(in_path: str, out_path: str) -> None:
 
-    with BZ2File(in_path, "rb") as in_file:
-        with multiprocessing.Pool(os.cpu_count()-1) as pool:
-            for i, lines in enumerate(pager(in_file)):
-                pool.apply_async(process_game, args=(str(lines), out_path))
+    try:
+        with BZ2File(in_path, "rb") as in_file:
+            with multiprocessing.Pool(os.cpu_count()-1) as pool:
+                for i, lines in enumerate(pager(in_file)):
+                    pool.apply_async(process_game, args=(str(lines), out_path))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        for out_file in out_files.values():
+            out_file.close()
 
 
 def unite(dir: str, out: str) -> None:
@@ -184,23 +73,6 @@ def unite(dir: str, out: str) -> None:
         for filename in os.listdir(dir):
             with open(os.path.join(dir, filename)) as inFile:
                 outfile.write(inFile.read())
-
-
-class Plotter(Callback):
-    batch_loss = []  # loss at given batch
-
-    def __init__(self, batches):
-        super(Plotter, self).__init__()
-        self.batches = batches
-        self.current_batch = 0
-
-    def on_train_batch_end(self, batch, logs=None):
-        self.current_batch += 1
-
-        Plotter.batch_loss.append(logs.get('loss'))
-
-        if self.current_batch % self.batches == 0:
-            plot(Plotter.batch_loss, Plotter.batch_loss, 'loss', '')
 
 
 def gen_model() -> Sequential:
@@ -286,21 +158,6 @@ def test_model():
         break
 
 
-def prep():
-    try:
-        preprocess("dataset/lichess_db_standard_rated_2021-10.pgn.bz2",
-                   "dataset/processed_games/nm_games.csv")
-    except KeyboardInterrupt:
-        pass
-    finally:
-        for out_file in out_files.values():
-            out_file.close()
-
-
-def combine():
-    unite("dataset/processed_games", "dataset/nm_games.csv")
-
-
 def learn():
     model = gen_model()
     model.summary()
@@ -314,8 +171,9 @@ def learn():
 
 
 def main():
-    prep()
-    combine()
+    preprocess("dataset/lichess_db_standard_rated_2021-10.pgn.bz2",
+               "dataset/processed_games/nm_games.csv")
+    unite("dataset/processed_games", "dataset/nm_games.csv")
     learn()
     test_model()
 
