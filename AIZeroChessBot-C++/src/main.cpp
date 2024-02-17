9a6dc2ff8a7a5660b0cb9773fb251684f337c462
Bertil Braun <bertil.braun.private@gmail.com>
2024-02-17 19:15:41 +0100

    Sample Chess usage

--- AIZeroChessBot-C++/src/main.cpp
+++ AIZeroChessBot-C++/src/main.cpp
@@ -1,8 +1,23 @@
+#include "chess.hpp"
 #include <iostream>
 
-int main()
-{
+
+using namespace chess;
+
+int main() {
 
     std::cout << "Hello, World!" << std::endl;
+
+    Board board;
+    std::cout << board.boardFen() << std::endl;
+    board.push(Move::fromUci("e2e4"));
+    std::cout << board.boardFen() << std::endl;
+
+    for (auto move : board.legalMoves()) {
+        std::cout << move.uci() << std::endl;
+    }
+
+    board.push(board.legalMoves()[0]);
+
     return 0;
 }
