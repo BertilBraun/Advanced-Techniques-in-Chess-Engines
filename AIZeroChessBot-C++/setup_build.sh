da1a3daedf8aac4b9f3fee0287e533f902414291
Bertil Braun <bertil.braun.private@gmail.com>
2024-02-18 23:14:14 +0100

    Testing with versions

--- AIZeroChessBot-C++/setup_build.sh
+++ AIZeroChessBot-C++/setup_build.sh
@@ -12,11 +12,11 @@
 #SBATCH --error=setup_%j.txt
 
 
-module load devel/cuda/11.8
+module load devel/cuda/12.0
 
 
 # Set LibTorch download URL
-LIBTORCH_URL="https://download.pytorch.org/libtorch/cu118/libtorch-shared-with-deps-2.2.0%2Bcu118.zip"
+LIBTORCH_URL="https://download.pytorch.org/libtorch/cu118/libtorch-cxx11-abi-shared-with-deps-2.2.0%2Bcu118.zip"
 LIBTORCH_ZIP="libtorch.zip"
 
 # Create a directory for LibTorch if it doesn't exist
