8c7231d4617d7f7baf3d2f096dd85b6c795f3b19
Bertil Braun <bertil.braun.private@gmail.com>
2024-02-20 20:49:59 +0100

    Update multinode_test.sh

--- AIZeroChessBot-C++/train/multinode_test.sh
+++ AIZeroChessBot-C++/train/multinode_test.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=00:05:00                    # wall-clock time limit
 #SBATCH --mem=2000                         # memory per node
 #SBATCH --nodes=2                          # number of nodes to be used
-#SBATCH --cpus-per-task=4                  # number of CPUs required per MPI task
+#SBATCH --cpus-per-task=1                  # number of CPUs required per MPI task
 #SBATCH --ntasks-per-node=3                # maximum count of tasks per node
 #SBATCH --output=multinode_test.txt
 #SBATCH --error=multinode_test.txt
