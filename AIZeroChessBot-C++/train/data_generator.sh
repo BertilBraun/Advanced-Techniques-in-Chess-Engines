e2a4d07c0b9032f8bf3ab850a751df837a943d97
Bertil Braun <bertil.braun.private@gmail.com>
2024-02-22 18:59:19 +0100

    Update data_generator.sh

--- AIZeroChessBot-C++/train/data_generator.sh
+++ AIZeroChessBot-C++/train/data_generator.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=generator               # job name
-#SBATCH --partition=single                 # mby GPU queue for the resource allocation.
+#SBATCH --partition=gpu_4                  # mby GPU queue for the resource allocation.
 #SBATCH --time=04:10:00                    # wall-clock time limit
 #SBATCH --mem=20000                        # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
 #SBATCH --cpus-per-task=1                  # number of CPUs required per MPI task
-#SBATCH --ntasks-per-node=20               # maximum count of tasks per node
+#SBATCH --ntasks-per-node=4               # maximum count of tasks per node
 #SBATCH --mail-type=ALL                    # Notify user by email when certain event types occur.
+#SBATCH --gres=gpu:0
 #SBATCH --output=generator_%j.txt
 #SBATCH --error=generator_%j.txt
 
