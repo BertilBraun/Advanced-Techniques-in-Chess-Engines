be0268a84c5f070747aef79a5ae2403fe667e569
Bertil Braun <bertil.braun.private@gmail.com>
2024-02-27 10:22:47 +0100

    Moved eval and changed to cpu libtorch for eval

---
+++ AIZeroChessBot-C++/eval/eval_setup_build.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#SBATCH --job-name=setup                   # job name
+#SBATCH --partition=dev_gpu_4              # mby GPU queue for the resource allocation.
+#SBATCH --time=00:05:00                    # wall-clock time limit
+#SBATCH --mem=100000                       # memory per node
+#SBATCH --nodes=1                          # number of nodes to be used
+#SBATCH --cpus-per-task=1                  # number of CPUs required per MPI task
+#SBATCH --ntasks-per-node=1                # maximum count of tasks per node
+#SBATCH --gres=gpu:1
+#SBATCH --output=setup_%j.txt
+#SBATCH --error=setup_%j.txt
+
+module purge
+module restore chess
+
+# The Structure after the setup is done is as follows:
+# eval
+# ├── build
+# │   └── EvalAIZeroChessBot
+# ├── libtorch
+# ├── models
+# │   └── stockfish-8-linux
+# ├── EvalAIZeroChessBot
+# └── eval_setup_build.sh
+
+
+# Set LibTorch download URL
+LIBTORCH_URL="https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-2.2.1%2Bcpu.zip"
+LIBTORCH_ZIP="libtorch.zip"
+
+# Create a directory for LibTorch if it doesn't exist
+mkdir -p libtorch
+
+# Download LibTorch only if it hasn't been downloaded yet
+if [ ! -f "libtorch/$LIBTORCH_ZIP" ]; then
+    echo "Downloading LibTorch..."
+    wget -O "libtorch/$LIBTORCH_ZIP" "$LIBTORCH_URL"
+    echo "Extracting LibTorch..."
+    unzip -o "libtorch/$LIBTORCH_ZIP" -d .
+fi
+
+# Download the model file
+mkdir -p models
+wget https://drive.usercontent.google.com/u/0/uc?id=1hhuy4O9grrqaL92hbFboMrwFnCZXNheq&export=download -o models/stockfish.zip
+unzip models/stockfish.zip -d models
+mv models/stockfish-8-linux/Linux/stockfish_8_x64 stockfish_8_x64
+rm -rf models/stockfish-8-linux
+rm models/stockfish.zip
+
+# Create a build directory
+mkdir -p build
+cd build
+
+# Configure the project with CMake
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DTorch_DIR=$(pwd)/../libtorch/share/cmake/Torch ..
+
+cmake --build . --config Release
+
+cp EvalAIZeroChessBot ../EvalAIZeroChessBot
+
+cd ..
+
+echo "Setup completed."
