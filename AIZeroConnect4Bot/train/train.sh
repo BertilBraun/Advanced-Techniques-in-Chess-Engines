31ed1d4fd90ea970994f6b259730c7a93a38a09b
Bertil Braun <bertil.braun.private@gmail.com>
2024-12-12 16:24:48 +0000

    Update train.sh

--- AIZeroConnect4Bot/train/train.sh
+++ AIZeroConnect4Bot/train/train.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=200000                       # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
 #SBATCH --cpus-per-task=1                  # number of CPUs required per MPI task
-#SBATCH --ntasks-per-node=1                # maximum count of tasks per node
+#SBATCH --ntasks-per-node=4                # maximum count of tasks per node
 #SBATCH --mail-type=ALL                    # Notify user by email when certain event types occur.
 #SBATCH --gres=gpu:4
 #SBATCH --output=train_zero_%j.txt
