ba61804df2a7bb58a16037793126f692559001e3
Bertil Braun <bertil.braun.private@gmail.com>
2024-12-16 00:18:25 +0000

    Speedups

--- AIZeroConnect4Bot/src/games/Board.py
+++ AIZeroConnect4Bot/src/games/Board.py
@@ -52,21 +52,14 @@ class Board(ABC, Generic[_Move]):
 
     @classmethod
     def _cache(cls):
-        cls.calls = {}
-        cls.hits = {}
-
         def cache_decorator(method):
             def cached_method(self, *args, **kwargs):
                 cache_name = f'_{method.__name__}_cached_result'
-                cls.calls[method.__name__] = cls.calls.get(method.__name__, 0) + 1
-                if not hasattr(self, cache_name):
+                if not hasattr(self, 'cache_attr_names'):
+                    setattr(self, 'cache_attr_names', [])
+                self.cache_attr_names.append(cache_name)
+                if not hasattr(self, cache_name) or getattr(self, cache_name) is None:
                     setattr(self, cache_name, method(self, *args, **kwargs))
-                else:
-                    cls.hits[method.__name__] = cls.hits.get(method.__name__, 0) + 1
-                # TODO remove cache stats
-                print(
-                    f'Cache stats for {method.__name__}: {cls.hits.get(method.__name__,0)}/{cls.calls[method.__name__]}'
-                )
 
                 return getattr(self, cache_name)
 
@@ -75,6 +68,7 @@ class Board(ABC, Generic[_Move]):
         return cache_decorator
 
     def _invalidate_cache(self) -> None:
-        for attr in dir(self):
-            if attr.endswith('_cached_result'):
-                delattr(self, attr)
+        if not hasattr(self, 'cache_attr_names'):
+            return
+        for attr in getattr(self, 'cache_attr_names'):
+            setattr(self, attr, None)
