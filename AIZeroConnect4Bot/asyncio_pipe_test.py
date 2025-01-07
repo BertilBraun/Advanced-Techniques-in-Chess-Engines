6516306d4e93f26d2d48f712fe659e5787afbb94
Bertil Braun <bertil.braun.private@gmail.com>
2025-01-07 17:56:26 +0000

    Update asyncio_pipe_test.py

--- AIZeroConnect4Bot/asyncio_pipe_test.py
+++ AIZeroConnect4Bot/asyncio_pipe_test.py
@@ -7,8 +7,15 @@ from asyncio_pipe import Connection
 async def verify_duplex_connection(async_connection, connection):
     connection.send([1, 2, 3])
     assert await async_connection.recv() == [1, 2, 3]
-    await async_connection.send([4, 5, 6])
+    async_connection.send([4, 5, 6])
     assert connection.recv() == [4, 5, 6]
+
+    # test bytes
+    connection.send_bytes(b'hello')
+    assert await async_connection.recv_bytes() == b'hello'
+    async_connection.send_bytes(b'world')
+    assert connection.recv_bytes() == b'world'
+
     connection.close()
     async_connection.close()
 
