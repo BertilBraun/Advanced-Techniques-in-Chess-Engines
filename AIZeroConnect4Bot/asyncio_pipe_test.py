245e1f3768ec03fda3b434c5c50c9ed2d938ab1d
Bertil Braun <bertil.braun.private@gmail.com>
2025-01-07 18:48:40 +0000

    Update asyncio_pipe_test.py

--- AIZeroConnect4Bot/asyncio_pipe_test.py
+++ AIZeroConnect4Bot/asyncio_pipe_test.py
@@ -12,16 +12,29 @@ from src.settings import CurrentBoard
 pipe1, pipe2 = multiprocessing.Pipe()
 comm1, comm2 = multiprocessing.Pipe()
 
+print('Starting inference server')
 p1 = multiprocessing.Process(target=run_inference_server, args=(pipe1, comm1, 0))
 p1.start()
 
+print('Sending start message')
+
 comm2.send('START AT ITERATION: 0')
+
+print('Creating client')
+
 client = InferenceClient(pipe2)
+
+print('Running inference')
 
 
 async def main():
-    await client.inference([CurrentBoard()])
+    print('Inference result:')
+    res = await client.inference([CurrentBoard()])
+    print(res)
+    print('Inference done')
 
 
 asyncio.run(main())
+
+print('Sending stop message')
 p1.join()
