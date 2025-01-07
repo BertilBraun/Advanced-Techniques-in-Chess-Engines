a41ca78c4a09c8e1de48ff8e960a9612cd9471a8
Bertil Braun <bertil.braun.private@gmail.com>
2025-01-07 17:59:44 +0000

    Update asyncio_pipe_test.py

--- AIZeroConnect4Bot/asyncio_pipe_test.py
+++ AIZeroConnect4Bot/asyncio_pipe_test.py
@@ -4,6 +4,22 @@ import multiprocessing
 from asyncio_pipe import Connection
 
 
+def send(connection, obj):
+    connection.send(obj)
+
+
+def recv(connection):
+    return connection.recv()
+
+
+def send_bytes(connection, buf, offset=0, size=None):
+    connection.send_bytes(buf, offset, size)
+
+
+def recv_bytes(connection, maxlength=None):
+    return connection.recv_bytes(maxlength)
+
+
 async def verify_duplex_connection(async_connection, connection):
     connection.send([1, 2, 3])
     assert await async_connection.recv() == [1, 2, 3]
@@ -14,6 +30,27 @@ async def verify_duplex_connection(async_connection, connection):
     connection.send_bytes(b'hello')
     assert await async_connection.recv_bytes() == b'hello'
     async_connection.send_bytes(b'world')
+    assert connection.recv_bytes() == b'world'
+
+    # test on other process
+    p = multiprocessing.Process(target=send, args=(connection, [1, 2, 3]))
+    p.start()
+
+    assert await async_connection.recv() == [1, 2, 3]
+
+    p = multiprocessing.Process(target=send_bytes, args=(connection, b'hello'))
+    p.start()
+
+    assert await async_connection.recv_bytes() == b'hello'
+
+    p = multiprocessing.Process(target=send, args=(async_connection, [4, 5, 6]))
+    p.start()
+
+    assert connection.recv() == [4, 5, 6]
+
+    p = multiprocessing.Process(target=send_bytes, args=(async_connection, b'world'))
+    p.start()
+
     assert connection.recv_bytes() == b'world'
 
     connection.close()
