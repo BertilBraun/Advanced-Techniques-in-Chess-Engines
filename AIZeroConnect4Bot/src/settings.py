c26e1fb750b491d5d04164b33aaab22fc4d9bf58
Bertil Braun <bertil.braun.private@gmail.com>
2025-01-09 22:26:42 +0000

    Update settings.py

--- AIZeroConnect4Bot/src/settings.py
+++ AIZeroConnect4Bot/src/settings.py
@@ -140,7 +140,7 @@ if True:
         ),
     )
     # TODO remove
-    TRAINING_ARGS = TrainingArgs(
+    TEST_TRAINING_ARGS = TrainingArgs(
         num_iterations=25,
         save_path=SAVE_PATH + '/connect4',
         num_games_per_iteration=32,
