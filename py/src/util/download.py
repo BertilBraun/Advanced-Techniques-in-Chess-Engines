b7034a8e54c687da5332a245505befd14116b3fb
Bertil Braun <bertil.braun.private@gmail.com>
2025-01-26 21:38:22 +0000

    Fix download to create output folder

--- py/src/util/download.py
+++ py/src/util/download.py
@@ -8,5 +8,6 @@ def download(url: str, save_path: str) -> None:
 
     print(f'Downloading {url}')
     r = requests.get(url)
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
     with open(save_path, 'wb') as f:
         f.write(r.content)
