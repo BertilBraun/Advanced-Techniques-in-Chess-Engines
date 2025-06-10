02b26bb79b2b248f7e0e9fa6b1b2cf50880c45e8
Bertil Braun <bertil.braun.private@gmail.com>
2025-06-10 10:25:54 +0200

    Update ChessSettings.py

--- py/src/games/chess/ChessSettings.py
+++ py/src/games/chess/ChessSettings.py
@@ -52,7 +52,7 @@ evaluation = EvaluationParams(
     dataset_path='reference/memory_0_chess_database.hdf5',
 )
 
-NUM_SELF_PLAYERS = 2
+NUM_SELF_PLAYERS = 1
 NUM_THREADS = int(64 // NUM_SELF_PLAYERS * 1.5)
 PARALLEL_GAMES = NUM_THREADS
 NUM_SEARCHES_PER_TURN = 800
@@ -77,7 +77,7 @@ if not USE_GPU:  # TODO remove
 TRAINING_ARGS = TrainingArgs(
     num_iterations=300,
     save_path=SAVE_PATH + '/chess',
-    num_games_per_iteration=PARALLEL_GAMES * NUM_SELF_PLAYERS * 8,
+    num_games_per_iteration=PARALLEL_GAMES * NUM_SELF_PLAYERS * 32,
     network=network,
     self_play=SelfPlayParams(
         num_parallel_games=PARALLEL_GAMES,
