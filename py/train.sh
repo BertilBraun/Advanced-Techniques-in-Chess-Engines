e3a5556bd4c0a19fb560c8dbe9993ecdef3b812f
Bertil Braun <bertil.braun.private@gmail.com>
2025-06-10 08:30:38 +0200

    Update train.sh

--- py/train.sh
+++ py/train.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=train_zero              # job name
 #SBATCH --partition=accelerated            # mby GPU queue for the resource allocation.
 #SBATCH --time=04:00:00                    # wall-clock time limit
-#SBATCH --mem=500000                       # memory per node
+#SBATCH --mem=200000                       # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
 #SBATCH --cpus-per-task=50                 # number of CPUs required per MPI task
 #SBATCH --ntasks-per-node=1                # maximum count of tasks per node
