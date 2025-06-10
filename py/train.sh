f28e341e02ac7dcbbba6ddb231e32e7a18ecd6e5
Bertil Braun <bertil.braun.private@gmail.com>
2025-06-10 08:28:22 +0200

    Update train.sh

--- py/train.sh
+++ py/train.sh
@@ -5,10 +5,10 @@
 #SBATCH --time=04:00:00                    # wall-clock time limit
 #SBATCH --mem=500000                       # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
-#SBATCH --cpus-per-task=122                # number of CPUs required per MPI task
+#SBATCH --cpus-per-task=50                 # number of CPUs required per MPI task
 #SBATCH --ntasks-per-node=1                # maximum count of tasks per node
 #SBATCH --mail-type=ALL                    # Notify user by email when certain event types occur.
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --output=train_zero_%j.txt
 #SBATCH --error=train_zero_%j.txt
 
