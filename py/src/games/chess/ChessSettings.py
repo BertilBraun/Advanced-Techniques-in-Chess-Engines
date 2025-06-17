1d1cd01326487cdb77328a30fde51a9b73823907
Bertil Braun <bertil.braun.private@gmail.com>
2025-06-17 20:23:38 +0200

    Reenable use of all gpus

--- py/src/games/chess/ChessSettings.py
+++ py/src/games/chess/ChessSettings.py
@@ -52,7 +52,7 @@ evaluation = EvaluationParams(
     dataset_path='reference/memory_0_chess_database.hdf5',
 )
 
-NUM_SELF_PLAYERS = 3 * max(torch.cuda.device_count() - 1, 1) if USE_GPU else 2
+NUM_SELF_PLAYERS = 3 * max(torch.cuda.device_count(), 1) if USE_GPU else 2
 NUM_THREADS = multiprocessing.cpu_count() // NUM_SELF_PLAYERS * 8
 PARALLEL_GAMES = 64
 NUM_SEARCHES_PER_TURN = 400  # More searches? 500-800? # NOTE: if KL divergence between policy and mcts policy is < 0.2 then add more searches
