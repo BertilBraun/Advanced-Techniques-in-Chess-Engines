55995a9edebecbe8223f8c0ee69778a566d8cf74
Bertil Braun <bertil.braun.private@gmail.com>
2025-06-17 13:28:26 +0200

    Update ChessSettings.py

--- py/src/games/chess/ChessSettings.py
+++ py/src/games/chess/ChessSettings.py
@@ -53,9 +53,9 @@ evaluation = EvaluationParams(
 )
 
 NUM_SELF_PLAYERS = 2 * torch.cuda.device_count() if USE_GPU else 2
-NUM_THREADS = multiprocessing.cpu_count() // NUM_SELF_PLAYERS * 3
+NUM_THREADS = multiprocessing.cpu_count() // NUM_SELF_PLAYERS * 5
 PARALLEL_GAMES = NUM_THREADS
-NUM_SEARCHES_PER_TURN = 256  # More searches? 500-800? # NOTE: if KL divergence between policy and mcts policy is < 0.2 then add more searches
+NUM_SEARCHES_PER_TURN = 320  # More searches? 500-800? # NOTE: if KL divergence between policy and mcts policy is < 0.2 then add more searches
 MIN_VISIT_COUNT = 1
 PARALLEL_SEARCHES = 4
 
