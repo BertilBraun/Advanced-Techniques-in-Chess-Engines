b0abfb58bb756d11681dbc89ab604e16ee71539c
Bertil Braun <bertil.braun.private@gmail.com>
2025-06-20 18:40:39 +0200

    Setup and Project cleanup

--- getting_started.sh
+++ getting_started.sh
@@ -7,56 +7,32 @@ git clone https://github.com/BertilBraun/Advanced-Techniques-in-Chess-Engines.gi
 
 cd Advanced-Techniques-in-Chess-Engines/py
 
-# if on aarch64
-if [[ "$(uname -m)" == "aarch64" ]]; then
-    echo "alias tail='tail -f -n 2000'" >> ~/.bashrc
-    echo "alias ch='chmod +x *.sh'" >> ~/.bashrc
-    echo "alias tb='tensorboard --port 6007 --logdir'" >> ~/.bashrc
-    echo "alias gp='git pull'" >> ~/.bashrc
-    echo "alias start='git pull && nohup python3 train.py > \"train_\$(date +%Y%m%d_%H%M%S).log\" 2>&1 &'" >> ~/.bashrc
-    echo "alias stop='pkill -f train.py'" >> ~/.bashrc
+source setup.sh
 
-    echo "cd ~/Advanced-Techniques-in-Chess-Engines/py" >> ~/.bashrc
+compile
 
-    python3 -m venv .venv
-    
-    echo "source .venv/bin/activate" >> ~/.bashrc
+python3 inference_speed_test.py
 
-    source ~/.bashrc
-        
-    pip3 install --upgrade pip
-    pip3 install -r requirements.txt
+# download the chess database
+python3 -m src.games.chess.ChessDatabase 5 2000
 
-    start
-    # tail train_$(date +%Y%m%d_%H%M%S).log
-else
-    source setup.sh
+mkdir -p training_data/chess
+# initialize the training_data with some of the chess database to start training
+mkdir -p training_data/chess/memory_0
+mv reference/chess_database/memory_202409/* training_data/chess/memory_0/
 
-    compile
+# pretrain the model on the chess database
+python3 -m src.eval.DatasetTrainer reference/chess_database/memory_*/*.hdf5
 
-    python3 inference_speed_test.py
+# copy the pretrained model to the training_data directory
+cp reference/ChessGame/model_5.pt training_data/chess/model_1.pt
+cp reference/ChessGame/model_5.jit.pt training_data/chess/model_1.jit.pt
+cp reference/ChessGame/model_5.pt training_data/chess/reference_model.pt
+cp reference/ChessGame/model_5.jit.pt training_data/chess/reference_model.jit.pt
 
-    # download the chess database
-    python3 -m src.games.chess.ChessDatabase 5 2000
+# run train.py based on the pretrained model
+python3 train.py
 
-    mkdir -p training_data/chess
-    # initialize the training_data with some of the chess database to start training
-    mkdir -p training_data/chess/memory_0
-    mv reference/chess_database/memory_202409/* training_data/chess/memory_0/
-
-    # pretrain the model on the chess database
-    python3 -m src.eval.DatasetTrainer reference/chess_database/memory_*/*.hdf5
-
-    # copy the pretrained model to the training_data directory
-    cp reference/ChessGame/model_5.pt training_data/chess/model_1.pt
-    cp reference/ChessGame/model_5.jit.pt training_data/chess/model_1.jit.pt
-    cp reference/ChessGame/model_5.pt training_data/chess/reference_model.pt
-    cp reference/ChessGame/model_5.jit.pt training_data/chess/reference_model.jit.pt
-    
-    # run train.py based on the pretrained model
-    python3 train.py
-
-    # Or run train.py in the background
-    start
-    # tail train_$(date +%Y%m%d_%H%M%S).log
-fi
+# Or run train.py in the background
+start
+# tail train_$(date +%Y%m%d_%H%M%S).log
--- py/setup.sh
+++ py/setup.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-#SBATCH --job-name=setup                 # job name
-#SBATCH --partition=dev_accelerated      # mby GPU queue for the resource allocation.
-#SBATCH --time=00:20:00                  # wall-clock time limit
-#SBATCH --mem=10000                      # memory per node
-#SBATCH --nodes=1                        # number of nodes to be used
-#SBATCH --cpus-per-task=1                # number of CPUs required per MPI task
-#SBATCH --ntasks-per-node=1              # maximum count of tasks per node
-#SBATCH --mail-type=ALL                  # Notify user by email when certain event types occur.
-#SBATCH --gres=gpu:1
-#SBATCH --output=setup.txt
-#SBATCH --error=setup.txt
-
-
 # if miniconda is not installed, install it and create a new conda environment
 if [ -d "../../miniconda3" ]; then
   echo "Conda environment already exists."
@@ -33,19 +20,16 @@ else
 
     # 1) add / refresh NVIDIA’s CUDA repo for Ubuntu 20.04
     sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
-    sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+    sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /" -y
 
     # 2) pull in NVTX + cuDNN 9 for CUDA 12.x
     CUDA_REL=12-8              # 12-6, 12-7, … use whatever tool-kit minor you’re building against
-    sudo apt-get install -y cuda-nvtx-${CUDA_REL} libcudnn9-cuda-12 libcudnn9-dev-cuda-12
+    sudo apt install cuda-nvtx-${CUDA_REL} libcudnn9-cuda-12 libcudnn9-dev-cuda-12 -y
 
     sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-    sudo apt-get update
     sudo apt update
-    sudo apt install gcc-11 g++-11 -y
+    sudo apt install gcc-11 g++-11 stockfish nano htop -y
 
-
-    sudo apt install stockfish nano htop -y
     echo "export PATH=\"\$PATH:/usr/games\"" >> ~/.bashrc
 
     echo "export CMAKE_INCLUDE_PATH=/usr/include/x86_64-linux-gnu:\$CMAKE_INCLUDE_PATH" >> ~/.bashrc
@@ -53,38 +37,42 @@ else
     echo "export PATH=/usr/local/cuda-12/bin:\$PATH" >> ~/.bashrc
     echo "export LD_LIBRARY_PATH=/usr/local/cuda-12/lib64:\$LD_LIBRARY_PATH" >> ~/.bashrc
 
-    pip3 install -r requirements.txt
-
-    # Add to .bashrc
-    echo "module purge" >> ~/.bashrc
-    echo "module load compiler/intel/2024.0_llvm" >> ~/.bashrc
-    echo "module load devel/cuda/12.4" >> ~/.bashrc
+    pip3 install -r ~/Advanced-Techniques-in-Chess-Engines/py/requirements.txt
     
+    # Add Slurm aliases to .bashrc
     echo "alias q='squeue --long'" >> ~/.bashrc
     echo "alias qs='watch \"squeue --start\"'" >> ~/.bashrc
     echo "alias qi='watch \"sinfo_t_idle && squeue && ls\"'" >> ~/.bashrc
     echo "alias b='sbatch'" >> ~/.bashrc
     echo "alias c='scancel'" >> ~/.bashrc
     echo "alias info='sinfo_t_idle'" >> ~/.bashrc
+
+    # Intereactive GPU allocation aliases for GPU Slurm Clusters
+    echo "alias gpuH='salloc -p dev_accelerated-h100 --gres=gpu:1 -t 30'" >>  ~/.bashrc
+    echo "alias gpuA='salloc -p dev_accelerated --gres=gpu:1 -t 30'" >> ~/.bashrc
+    echo "alias gpuH2='salloc -p dev_accelerated-h100 --gres=gpu:2 -t 30'" >> ~/.bashrc
+    echo "alias gpuA2='salloc -p dev_accelerated --gres=gpu:2 -t 30'" >>  ~/.bashrc
+
+    # Add compile aliases to .bashrc
+    echo "alias compile='cd ../cpp && mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=\$(which python3.10) && make -j && cd ../../py'" >> ~/.bashrc
+    echo "alias compileDebug='cd ../cpp && mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Debug -DPYTHON_EXECUTABLE=\$(which python3.10) && make -j && cd ../../py'" >> ~/.bashrc
+    echo "alias compileRel='cd ../cpp && mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DPYTHON_EXECUTABLE=\$(which python3.10) && make -j && cd ../../py'" >> ~/.bashrc
+    echo "alias compileOnly='cd ../cpp && mkdir -p build && cd build && make -j && cd ../../py'" >> ~/.bashrc
+
+    # Add aliases for starting and stopping the training script in a detached mode
+    echo "alias start='git pull && nohup python3 -O train.py > \"train_\$(date +%Y%m%d_%H%M%S).log\" 2>&1 &'" >> ~/.bashrc
+    echo "alias stop='pkill -f train.py && pkill -f \"python3 -O -c from multiprocessing.spawn import spawn_main\"'" >> ~/.bashrc
+
+    # Add general aliases to .bashrc
     echo "alias python='python3'" >> ~/.bashrc
     echo "alias pip='pip3'" >> ~/.bashrc
     echo "alias tail='tail -f -n 2000'" >> ~/.bashrc
     echo "alias ch='chmod +x *.sh'" >> ~/.bashrc
     echo "alias tb='ulimit -n 50000 && tensorboard --port 6007 --logdir'" >> ~/.bashrc
     echo "alias gp='git pull'" >> ~/.bashrc
-    echo "alias start='git pull && nohup python3 -O train.py > \"train_\$(date +%Y%m%d_%H%M%S).log\" 2>&1 &'" >> ~/.bashrc
-    echo "alias stop='pkill -f train.py && pkill -f \"python3 -O -c from multiprocessing.spawn import spawn_main\"'" >> ~/.bashrc
-    echo "alias gpuH='salloc -p dev_accelerated-h100 --gres=gpu:1 -t 30'" >>  ~/.bashrc
-    echo "alias gpuA='salloc -p dev_accelerated --gres=gpu:1 -t 30'" >> ~/.bashrc
-    echo "alias gpuH2='salloc -p dev_accelerated-h100 --gres=gpu:2 -t 30'" >> ~/.bashrc
-    echo "alias gpuA2='salloc -p dev_accelerated --gres=gpu:2 -t 30'" >>  ~/.bashrc
-    echo "alias compile='cd ../cpp && mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=\$(which python3.10) && make -j && cd ../../py'" >> ~/.bashrc
-    echo "alias compileDebug='cd ../cpp && mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Debug -DPYTHON_EXECUTABLE=\$(which python3.10) && make -j && cd ../../py'" >> ~/.bashrc
-    echo "alias compileRel='cd ../cpp && mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DPYTHON_EXECUTABLE=\$(which python3.10) && make -j && cd ../../py'" >> ~/.bashrc
-    echo "alias compileOnly='cd ../cpp && mkdir -p build && cd build && make -j && cd ../../py'" >> ~/.bashrc
 
+    # Set ulimit for open files
     echo "ulimit -n 4096" >> ~/.bashrc
-    
     
 
     echo "set -g mouse on #For tmux version 2.1 and up" >> ~/.tmux.conf
