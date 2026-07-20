71ce98e62ee3dd895e964bee5d666248d0fe83ec
Bertil Braun <bertil.braun.private@gmail.com>
2026-07-20 14:41:04 +0200

    Reduce training memory overlap

--- py/test/test_run_configuration.py
+++ py/test/test_run_configuration.py
@@ -298,7 +298,7 @@ def test_rule_complete_main_applies_training_and_monitoring_schedule() -> None:
     assert arguments.self_play_search_warmup_iterations == 15
     assert arguments.self_play_value_warmup_iterations == 30
     assert arguments.self_play_endgame_shortcut_fade_iterations == 50
-    assert arguments.self_play.inference_cache_capacity == 1_000_000
+    assert arguments.self_play.inference_cache_capacity == 700_000
     assert arguments.self_play.mcts.num_threads == 3
     assert arguments.cluster.self_play_tensorboard_processes == 1
     assert arguments.cluster.evaluation_device_cycle == (0, 1, 2, 3)
@@ -307,7 +307,7 @@ def test_rule_complete_main_applies_training_and_monitoring_schedule() -> None:
     assert arguments.evaluation is not None
     assert arguments.evaluation.num_games == 100
     assert arguments.evaluation.every_n_iterations == 2
-    assert arguments.evaluation.max_concurrent_tasks == 16
+    assert arguments.evaluation.max_concurrent_tasks == 4
     assert arguments.evaluation.previous_model_offsets == (5, 10)
     assert arguments.evaluation.historical_model_rotation_period == 5
     assert arguments.evaluation.stockfish_skill_levels == (0, 1, 2, 3)
